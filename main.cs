4a5cb8c71f5d81855f0abe9373fa6b8abef22af3
Kase1337 <linkinusf1@gmail.com>
2024-05-31 06:07:32 +0000

    Fixed logic typo

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    
     //declare the required variables
     double Measurement_in_inches  = 2.54;
     int Input_value;
@@ -16,6 +17,9 @@ class Program {
     double centimeters = Input_value  * Measurement_in_inches;
 
     // output the result of the conversion to the console.
-    Console.WriteLine((Input_value ) + " inches is "+ (centimeters)+ " centimeters");
-  }
+   Console.WriteLine((Input_value ) + " inches is "+ (centimeters)+ " centimeters");
+   
+
+   
+    }
 }
